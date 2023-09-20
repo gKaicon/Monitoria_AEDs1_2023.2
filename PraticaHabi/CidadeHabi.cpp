c586c01295684481075fde4ba13fadf7673cdf0f
Gabriel Káicon <gkaicon2@gmail.com>
2023-09-20 14:40:41 -0300

    Att 20/09 Lab5

--- PraticaHabi/CidadeHabi.cpp
+++ PraticaHabi/CidadeHabi.cpp
@@ -12,34 +12,17 @@ typedef struct Habitante
     char sexo;
 };
 
-int main()
+const int qtdH = 5;
+Habitante h[qtdH];
+double somaSa, somaFi, mediaSa, mediaFi, maiorSa, percMulher, qtdMulher = 0;
+
+void formataDecimal(){
+    cout.precision(3);
+    cout << fixed;
+}
+
+void PreencheSruct()
 {
-    UINT CPAGE_UTF8 = 65001;
-    UINT CPAGE_DEFAULT = GetConsoleOutputCP();
-    SetConsoleOutputCP(CPAGE_UTF8);
-
-    double somaSa, somaFi, mediaSa, mediaFi, maiorSa, percMulher, qtdMulher = 0;
-    int qtdH = 5;
-    Habitante h[qtdH];
-
-    ofstream escrita;
-    ifstream leitura;
-
-    escrita.open("ListaDados.txt");
-
-    for (int i = 0; i < qtdH; i++)
-    {
-        cout << "Registro " << i+1 << "° \n";
-        cout << "Informe o salário: R$ ";
-        cin >> h[i].salario;
-        cout << "Informe a idade: ";
-        cin >> h[i].idade;
-        cout << "Informe a quantidade de filhos: ";
-        cin >> h[i].numFilho;
-        cout << "Informe o sexo: ";
-        cin >> h[i].sexo;
-    }
-
     h[0].salario = 990.0;
     h[0].idade = 17;
     h[0].numFilho = 0;
@@ -69,16 +52,19 @@ int main()
     h[5].idade = 24;
     h[5].numFilho = 2;
     h[5].sexo = 'M';
+}
 
+void calculaRelatorio()
+{
     // média de salário da população
     // média de número de filhos da população
     // percentual de mulheres com salário abaixo de 1000
     // maior salário
-    for (int i = 0; i < qtdH; i++)
+    for (int i = 0; i < 5; i++)
     {
         somaFi += h[i].numFilho;
         somaSa += h[i].salario;
-        if (((h[i].sexo == 'F') || (h[i].sexo == 'f')) && (h[i].salario <= 1000))
+        if (((h[i].sexo == 'F') || (h[i].sexo == 'f')) && (h[i].salario > 1000))
             qtdMulher++;
         if (h[i].salario > maiorSa)
             maiorSa = h[i].salario;
@@ -86,9 +72,16 @@ int main()
     mediaSa = somaSa / qtdH;
     mediaFi = somaFi / qtdH;
     percMulher = qtdMulher * 100 / qtdH;
+}
 
-    //impressão
-    system("cls");    
+void MostraRelatorio(){
+    cout << "Média de Salário da população: R$ " << mediaSa << "\n";
+    cout << "Média de números de filhos da população: " << mediaFi << "\n";
+    cout << "Percentual de mulheres com salário acima de R$ 1000.00:  " << percMulher << "% \n";
+    cout << "Maior Salário: R$ " << maiorSa << endl;
+}
+
+void MostrarRegistros(){
     for (int i = 0; i < qtdH; i++)
     {
         cout << "\nRegistro " << i + 1 << "° \n";
@@ -97,9 +90,95 @@ int main()
         cout << "\tQuantidade de filhos: " << h[i].numFilho << endl;
         cout << "\tSexo: " << h[i].sexo << endl;
     }
-    cout << "Média de Salário da população: R$ " << mediaSa << "\n";
-    cout << "Média de números de filhos da população: " << mediaFi << "\n";
-    cout << "Percentual de mulheres com salário abaixo de R$ 1000.00:  " << percMulher << "% \n";
-    cout << "Maior Salário: R$ " << maiorSa << endl;
+}
+
+int main()
+{
+    UINT CPAGE_UTF8 = 65001;
+    UINT CPAGE_DEFAULT = GetConsoleOutputCP();
+    SetConsoleOutputCP(CPAGE_UTF8);
+
+    ofstream escrita;
+    ifstream leitura;
+
+    int op;
+    do
+    {
+        cout << "Deseja: \n1 - Prosseguir \n2 - Carregar os dados do arquivo\nOpção: ";
+        cin >> op;
+    } while (op != 1 && op != 2);
+
+    if (op == 1)
+    {
+
+        for (int i = 0; i < qtdH; i++)
+        {
+            cout << "Registro " << i + 1 << "° \n";
+            cout << "Informe o salário: R$ ";
+            cin >> h[i].salario;
+            cout << "Informe a idade: ";
+            cin >> h[i].idade;
+            cout << "Informe a quantidade de filhos: ";
+            cin >> h[i].numFilho;
+            cout << "Informe o sexo: ";
+            cin >> h[i].sexo;
+        }
+        calculaRelatorio();
+        MostraRelatorio();
+        // impressão
+        system("cls");
+        do
+        {
+            cout << "Deseja salvar as informações no arquivo?\n 1 - Sim\n 2 - Não\nOpção: ";
+            cin >> op;
+        } while (op != 1 && op != 2);
+
+        if (op == 1)
+        {
+            escrita.open("Relatorio.txt");
+            escrita << "Média de Salário da população: R$ " << mediaSa << "\n";
+            escrita << "Média de números de filhos da população: " << mediaFi << "\n";
+            escrita << "Percentual de mulheres com salário abaixo de R$ 1000.00:  " << percMulher << "% \n";
+            escrita << "Maior Salário: R$ " << maiorSa << endl;
+            escrita.close();
+
+            escrita.open("ListaDados.txt");
+            for (int i = 0; i < qtdH; i++)
+            {
+                escrita << h[i].salario << endl;
+                escrita << h[i].idade << endl;
+                escrita << h[i].numFilho << endl;
+                escrita << h[i].sexo << endl;
+            }
+            escrita.close();
+        }
+        if (op == 2)
+        {
+            cout << "Saindo...";
+            Sleep(3500);
+        }
+    }
+    if (op == 2)
+    {
+        leitura.open("ListaDados.txt"); // Abre o arquivo em modo binario, ou cria se não existir
+
+        if (leitura.fail())
+        {                    // Se falhar
+            cout << "ERROR!";
+            leitura.clear(); // Fecha
+            return false;
+        }
+
+        while (!leitura.eof())
+        { // Enquanto o arquivo não for vazio
+            for (int i = 0; i < 5; i++)
+            {
+                leitura >> h[i].salario >> h[i].idade >> h[i].numFilho >> h[i].sexo;
+            }
+        }
+        leitura.close(); // Fecha após carregado
+        calculaRelatorio();
+        MostraRelatorio();
+    }
     return 0;
 }
